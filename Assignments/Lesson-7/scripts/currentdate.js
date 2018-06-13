ac9754768148c40ecd011f200e32def31332d342
Ronald Silva <sil17021@byui.edu>
2018-06-12 20:01:08 -0600

    update

--- Assignments/Lesson-7/scripts/currentdate.js
+++ Assignments/Lesson-7/scripts/currentdate.js
@@ -1,2 +1,11 @@
 var d = new Date();
-document.getElementById("currentdate").innerHTML = d;
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+var day = [d.getDay()];
+var year = [d.getFullYear()];
+var daysnames = days[d.getDay()];
+var monthsnames = months[d.getMonth()];
+
+document.getElementById("currentdate").innerHTML = daysnames + ", " + day + " " + monthsnames + " " + year;
