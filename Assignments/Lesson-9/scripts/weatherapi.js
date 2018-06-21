9446813c009ce964b650800dc189d429ef43fcde
Ronald Silva <sil17021@byui.edu>
2018-06-20 21:55:06 -0600

    weather

--- Assignments/Lesson-9/scripts/weatherapi.js
+++ Assignments/Lesson-9/scripts/weatherapi.js
@@ -1,7 +1,7 @@
 var weatherRequest = new XMLHttpRequest();
+var apiURL = 'https://api.openweathermap.org/data/2.5/weather?&id=4156210&units=imperial&APPID=079794776d04eb4e82f25d2103f2ce58';
 
-
-weatherRequest.open('GET','https://api.openweathermap.org/data/2.5/weather?&id=4156210&units=imperial&APPID=079794776d04eb4e82f25d2103f2ce58', true);
+weatherRequest.open('GET', apiURL, true);
 
 weatherRequest.send();
 
@@ -9,14 +9,12 @@ weatherRequest.onload = function() {
    var weatherInfo = JSON.parse(weatherRequest.responseText);
     console.log(weatherInfo);
     
-     document.getElementById('city').innerHTML = weatherInfo.name;
-     document.getElementById('current').innerHTML = weatherInfo.main.temp;
+    document.getElementById('current').innerHTML = weatherInfo.main.temp;
      document.getElementById('description').innerHTML = weatherInfo.weather["0"].description;
     document.getElementById('hightemp').innerHTML = weatherInfo.main.temp_max;
     document.getElementById('lowtemp').innerHTML = weatherInfo.main.temp_min;
     document.getElementById('windspeed').innerHTML = weatherInfo.wind.speed;
    
-    var icon = 'http://openweathermap.org/img/w/' + weatherInfo.weather["0"].icon + '.png';
-    document.getElementById('weatherimage').src = icon;
+    var imagesrc = 'http://openweathermap.org/img/w/' + weatherData.weather[0].icon + '.png';
+    document.getElementById('weatherimage').src = imagesrc;
 }
- 
