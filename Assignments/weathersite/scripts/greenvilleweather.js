6d1c403ac821b5105753e6cb7e7a454d451f9f94
Ronald Silva <sil17021@byui.edu>
2018-06-26 20:40:43 -0600

    week 10

--- Assignments/weathersite/scripts/greenvilleweather.js
+++ Assignments/weathersite/scripts/greenvilleweather.js
@@ -1,7 +1,7 @@
 var weatherRequest = new XMLHttpRequest();
 
 
-weatherRequest.open('GET','https://api.openweathermap.org/data/2.5/weather?&id=4759986&units=imperial&APPID=079794776d04eb4e82f25d2103f2ce58', true);
+weatherRequest.open('GET','https://api.openweathermap.org/data/2.5/weather?&id=4695066&units=imperial&APPID=079794776d04eb4e82f25d2103f2ce58', true);
 
 weatherRequest.send();
 
